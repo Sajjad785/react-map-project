8bd45a659f3786d99eacf5ee2f9da6463d316f6e
ahmed.aftab <ahmed.aftab@rolustech.net>
2022-10-14 17:18:27 +0500

    Head blck c]

--- src/Details/LocDetail.jsx
+++ src/Details/LocDetail.jsx
@@ -3,29 +3,33 @@ import { Card,Row,Col } from 'react-bootstrap';
 import { ViserArea } from '../Chart/Viser';
 import brandIcon from '../assets/images/iconsBrandBra05.png'
 import './LocDetail.css'
+import HeadBlock from '../block/headblock';
+import moment from 'moment/moment';
 
 const LocationDetail = (props) => {
-    const { title,locData } = props;
+    const { block,title,locData } = props;
     return (
       <>
         <div className='loc-detail-container'>
-            <h1>Location {title}</h1>
+            <div>
+              <HeadBlock {...block}/>
+            </div>
             <div>
             {locData.map((item, i) => (
                 <Card className='loc-detail-card' key={i}>
                     <Card.Body>
                         <Row className='card-row'>
-                          <Col md={3} xs={12} className="icon-info-col">
+                          <Col lg={3} md={6} xs={12} className="icon-info-col">
                               <img className='brand-img' src={brandIcon} alt="" />
                               <div>
                               <p className='mb-1'>{item.name.substring(0,30)}</p>
                               <p>{item.email}</p>
                               </div>
                           </Col>
-                          <Col xs={12} md={2} className="time-col">
-                              <p>A month ago</p>
+                          <Col xs={12} md={6} lg={2} className="time-col">
+                              <p>{moment("2022-04-27T14:42:12").fromNow()}</p>
                           </Col>
-                          <Col xs={12} md={7}>
+                          <Col xs={12} md={12} lg={7}>
                           <ViserArea />
                           </Col>
                         </Row>
